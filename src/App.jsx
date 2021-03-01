bbc90f155d6c8457b0394327f6df78f69c30a636
mauricio403 <mauriciomatango_@hotmail.com>
2021-03-01 12:13:45 -0500

    empezanod

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@material-ui/core/Button'
+
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>material desing</h1>
+       <Button  variant="contained"color="secondary">Este es un botton</Button>
+       
+    </div>
+  );
+}
+
+export default App;
+
